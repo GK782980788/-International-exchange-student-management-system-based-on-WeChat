3efcf44a1c8f8d66d75f9e3cac9508af9835f639
GK782980788 <782980788@qq.com>
2016-11-23 11:07:47 +0800

    fix delete

--- iesssbow/src/com/lmmf/course/entity/LunBoTu.java
+++
@@ -1,9 +0,0 @@
-package com.lmmf.course.entity;
-/**
- * 由岳伟玺完成
- * 实现首页轮播图
- * 需要有增删改查功能
- */
-public class LunBoTu {
-
-}
--- iesssbow/src/com/lmmf/course/entity/Student.java
+++
@@ -1,8 +0,0 @@
-package com.lmmf.course.entity;
-/**
- * 由王鹏帆完成
- * 实现用户第一次使用微信记录相关信息
- */
-public class Student {
-
-}
